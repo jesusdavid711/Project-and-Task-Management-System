cf525cb3b537859503ee2eae0868210dfb76da6a
jesusdavid711 <jdcastro564@gmail.com>
2026-01-05 14:24:46 -0500

    chore: initial spring boot project setup

---
+++ SGPT/src/main/java/com/example/SGPT/SgptApplication.java
@@ -0,0 +1,13 @@
+package com.example.SGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgptApplication.class, args);
+	}
+
+}
